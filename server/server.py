18fa8a52f99fe2b3a24d92a0216a3ec4d445632b
Chase Struse <chasestruse@gmail.ocm>
2025-11-28 14:39:35 -0600

    Got the more specific GET to work

--- server/server.py
+++ server/server.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from server.messaging.messaging_service import get_last_ten_messages, insert_message
+from server.messaging.messaging_service import get_last_ten_messages, insert_message, get_all_messages_from_sender_id
 from server.models.message import Message
 
 app = FastAPI()
@@ -15,8 +15,11 @@ async def root():
 async def get_messages():
 	return get_last_ten_messages("test")
 
-
 # POST /message
 @app.post("/message")
 async def post_message(req_body: dict):
       return insert_message(req_body=req_body)
+
+@app.post("/message/reciever")
+async def get_messages_from_sender(req_body: dict):
+      return get_all_messages_from_sender_id(req_body=req_body)
