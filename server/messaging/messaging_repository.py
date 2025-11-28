f137639b51ac0212ef283e77a71769e39c51a4dc
Chase Struse <31481711+ChaseStruse@users.noreply.github.com>
2025-11-28 14:47:35 -0600

    Messaging (#3)
    * Added env vars
    
    * Got the sqlite db to work
    
    * Got the more specific GET to work
    
    ---------
    
    Co-authored-by: Chase Struse <chasestruse@gmail.ocm>

--- server/messaging/messaging_repository.py
+++ server/messaging/messaging_repository.py
@@ -1,12 +1,24 @@
 import os
+import uuid
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker
 from server.models.message import Base, Message
 
 load_dotenv()
+
+def to_dict(message: Message):
+    # Should be class function, not sure why it wasnt working...
+	print(message)
+	return {
+		"id": str(message.id),
+		"sender_id": str(message.sender_id),
+		"recipient_id": str(message.recipient_id),
+		"message": message.message,
+		"timestamp": message.timestamp.isoformat()
+	}
 
 def create_tables(engine):
     Base.metadata.create_all(bind=engine)
@@ -20,6 +32,17 @@ def get_all_items_from_db():
     with SessionLocal() as session:
         items = session.query(Message).all()
         return items
+    
+def get_all_messages_from_sender(sender_id: uuid.UUID, recipient_id: uuid.UUID):
+    SessionLocal = sessionmaker(bind=get_engine())
+    with SessionLocal() as session:
+        statement = select(Message).where(
+            (Message.recipient_id == recipient_id) & (Message.sender_id == sender_id)
+        )
+        result = session.execute(statement=statement).scalars().all()
+        print(type(result[0]))
+        messages = [to_dict(msg) for msg in result]
+    return messages
     
 def insert_item_to_db(item):
     SessionLocal = sessionmaker(bind=get_engine())
--- server/messaging/messaging_service.py
+++ server/messaging/messaging_service.py
@@ -1,11 +1,16 @@
 import uuid
 import datetime
 from server.models.message import Message
-from server.messaging.messaging_repository import insert_item_to_db, get_all_items_from_db
+from server.messaging.messaging_repository import insert_item_to_db, get_all_items_from_db, get_all_messages_from_sender
 
 def get_last_ten_messages(user_id: str):
 	return get_all_items_from_db()
 
+def get_all_messages_from_sender_id(req_body: dict):
+	sender_id = uuid.UUID(req_body.get("sender_id"))
+	recipient_id = uuid.UUID(req_body.get("recipient_id"))
+	messages = get_all_messages_from_sender(sender_id=sender_id, recipient_id=recipient_id)
+	return messages
 
 def insert_message(req_body: dict):
 	message = Message(
